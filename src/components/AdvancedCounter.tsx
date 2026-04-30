7cc8104bab4d48ffcaf5ede3e45956c8464bbeb9
Shanu Priya <priyads12@gmail.com>
2026-04-30 12:16:37 -0400

    Handled Arrow up and down key for increent and decrement counter

--- src/components/AdvancedCounter.tsx
+++ src/components/AdvancedCounter.tsx
@@ -1,43 +1,67 @@
 import React, { useEffect, useState } from "react";
 
-function AdvancedCounter(){
+function AdvancedCounter() {
 
     //define the state
     const [count, setCount] = useState(0);
     const [countHistory, setCountHistory] = useState<number[]>([]);
     const [stepValue, setSetValue] = useState(1);
 
-    
-    const handleDecrement = ()=>{
-        setCount(count-stepValue);
-           setCountHistory(prevHistory =>[...prevHistory, count]);
-        console.log(countHistory);
-    
+
+    const handleDecrement = () => {
+        decrementCounter();
     }
 
-    const handleIncrement = ()=>{
-        console.log(stepValue)
-        setCount(count+stepValue);
-        setCountHistory(prevHistory =>[...prevHistory, count]);
-        console.log(countHistory);
-    
+    const handleIncrement = () => {
+        incrementCounter();
     }
-    const updateStepValue=(e: React.ChangeEvent<HTMLInputElement>)=>{
+
+    const updateStepValue = (e: React.ChangeEvent<HTMLInputElement>) => {
         console.log(e.target.value);
         setSetValue(Number(e.target.value));
+    }
+
+    const handleReset = () => {
+        //reset counter and history
+        setCount(0);
+        setCountHistory([]);
+    }
+
+    const handleArrowUpKey = (e: React.KeyboardEvent) => {
+        if (e.key === "ArrowUp") 
+            incrementCounter();
      }
 
-    //let countHistoryArray = countHistory;
+    const handleArrowDownKey = (e: React.KeyboardEvent) => {
+        if (e.key === "ArrowDown")
+            decrementCounter();
+    }
 
-    useEffect(()=>{
-      // setCountHistory(...countHistoryArray, countHistoryArray.push(count)); 
-    },[count]);
-    return(<>
-    <h3>Counter</h3>
-    <h2>Current Count: {count}</h2>
-    <button type="button" onClick={handleDecrement}>Decrement</button>
-      <button type="button" onClick={handleIncrement}>Increment</button>
-        <button type="button" onClick={handleDecrement}>Reset</button>
+    const decrementCounter = () => {
+        console.log("Decrementing counter and update history")
+        setCount(count - stepValue);
+        setCountHistory(prevHistory => [...prevHistory, count]);
+        console.log(`Count History Array: ${countHistory}`);
+    }
+
+    const incrementCounter = () => {
+        console.log("Incrementing counter and update history")
+        setCount(count + stepValue);
+        setCountHistory(prevHistory => [...prevHistory, count]);
+        console.log(`Count History Array: ${countHistory}`);
+    }
+
+    useEffect(() => {
+        return (() => {
+
+        })
+    }, [count]);
+    return (<>
+        <h3>Counter</h3>
+        <h2>Current Count: {count}</h2>
+        <button type="button" onClick={handleDecrement} onKeyDown={handleArrowDownKey}>Decrement</button>
+        <button type="button" onClick={handleIncrement} onKeyDown={handleArrowUpKey}>Increment</button>
+        <button type="button" onClick={handleReset}>Reset</button>
         <div>
             <label>Step Value:</label>
             <input type="number" min={1} onChange={updateStepValue} value={stepValue}></input>
@@ -45,10 +69,10 @@ function AdvancedCounter(){
         </div>
         <div>
             <h5>Count History: {countHistory.length}</h5>
-            {countHistory.map((history)=> <div>{history}</div>)}
+            {countHistory.map((history) => <div>{history}</div>)}
         </div>
         <span>Use ArrowUp to increment and ArrowDown to decrement.</span>
     </>)
-}   
+}
 
 export default AdvancedCounter;
