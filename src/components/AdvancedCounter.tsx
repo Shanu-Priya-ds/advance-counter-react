8d3395b52a676ece62947fdfadde58bb94f98893
Shanu Priya <priyads12@gmail.com>
2026-04-30 09:55:55 -0400

    Populate the counter history using array map

--- src/components/AdvancedCounter.tsx
+++ src/components/AdvancedCounter.tsx
@@ -4,26 +4,34 @@ function AdvancedCounter(){
 
     //define the state
     const [count, setCount] = useState(0);
-    const [countHistory, setCountHistory] = useState([]);
+    const [countHistory, setCountHistory] = useState<number[]>([]);
     const [stepValue, setSetValue] = useState(1);
 
     
     const handleDecrement = ()=>{
         setCount(count-stepValue);
+           setCountHistory(prevHistory =>[...prevHistory, count]);
+        console.log(countHistory);
+    
     }
 
     const handleIncrement = ()=>{
         console.log(stepValue)
         setCount(count+stepValue);
+        setCountHistory(prevHistory =>[...prevHistory, count]);
+        console.log(countHistory);
+    
     }
     const updateStepValue=(e: React.ChangeEvent<HTMLInputElement>)=>{
         console.log(e.target.value);
         setSetValue(Number(e.target.value));
-    }
+     }
+
+    //let countHistoryArray = countHistory;
 
     useEffect(()=>{
-
-    });
+      // setCountHistory(...countHistoryArray, countHistoryArray.push(count)); 
+    },[count]);
     return(<>
     <h3>Counter</h3>
     <h2>Current Count: {count}</h2>
@@ -36,7 +44,8 @@ function AdvancedCounter(){
             <></>
         </div>
         <div>
-            <h5>Count History:</h5>
+            <h5>Count History: {countHistory.length}</h5>
+            {countHistory.map((history)=> <div>{history}</div>)}
         </div>
         <span>Use ArrowUp to increment and ArrowDown to decrement.</span>
     </>)
