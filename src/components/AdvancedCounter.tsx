d47ec357e57019afd0b6897fa296ecbd30c15d83
Shanu Priya <priyads12@gmail.com>
2026-04-30 08:55:03 -0400

    updated stepValue to the state, added increment decrement function

--- src/components/AdvancedCounter.tsx
+++ src/components/AdvancedCounter.tsx
@@ -1,13 +1,24 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function AdvancedCounter(){
 
     //define the state
     const [count, setCount] = useState(0);
     const [countHistory, setCountHistory] = useState([]);
+    const [stepValue, setSetValue] = useState(1);
 
+    
     const handleDecrement = ()=>{
+        setCount(count-stepValue);
+    }
 
+    const handleIncrement = ()=>{
+        console.log(stepValue)
+        setCount(count+stepValue);
+    }
+    const updateStepValue=(e: React.ChangeEvent<HTMLInputElement>)=>{
+        console.log(e.target.value);
+        setSetValue(Number(e.target.value));
     }
 
     useEffect(()=>{
@@ -17,11 +28,11 @@ function AdvancedCounter(){
     <h3>Counter</h3>
     <h2>Current Count: {count}</h2>
     <button type="button" onClick={handleDecrement}>Decrement</button>
-      <button type="button" onClick={handleDecrement}>Increment</button>
+      <button type="button" onClick={handleIncrement}>Increment</button>
         <button type="button" onClick={handleDecrement}>Reset</button>
         <div>
             <label>Step Value:</label>
-            <input type="number" min={1}></input>
+            <input type="number" min={1} onChange={updateStepValue} value={stepValue}></input>
             <></>
         </div>
         <div>
